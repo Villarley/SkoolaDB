f964f2fd6b74c6b54bb57ce47632d14920083082
Joaco <115122095+Villarley@users.noreply.github.com>
2024-06-10 14:51:59 -0600

    Adding cors

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,25 +1,29 @@
 import express,  { Application } from "express"
+import cors from "cors"
 class Server {
     private app: Application
     private port:string
     private apiPaths = {
-        auth : "/Skoola/Auth",
+        Auth : "/Skoola/Auth",
         Person : "/Skoola/Person",
-        Student : "/Skoola/AutStudent",
+        Student : "/Skoola/AuthStudent",
 
     }
     constructor(){
         this.app = express()
-        this.port = process.env["PORT"] || "8080";
+        this.port = process.env["PORT"] || "8080"
     }
 
     middlewares(){
-
+        //CORS
+        this.app.use( cors())
+        //lectura del body
+        this.app.use( express.json())
     }
     routes() {
     }
     listen() {
-        this.app.listen(this.port, ()=>{
+        this.app.listen(this.port, () =>  {
             console.log(`Server is running on: ${this.port}`)
         })
     }
