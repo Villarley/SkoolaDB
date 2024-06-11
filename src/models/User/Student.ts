2ac145b12490692a5236dda41187596b6dbd3afb
Joaco <115122095+Villarley@users.noreply.github.com>
2024-06-11 08:49:09 -0600

    Corrected imports

--- src/models/User/Student.ts
+++ src/models/User/Student.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, BeforeInsert, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm"
+import { Entity, BeforeInsert, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm"
 import { v4 as uuidv4 } from "uuid"
 import { User } from "./User"
 
